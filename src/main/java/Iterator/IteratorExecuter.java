60d217735af916d8231ea8589cf3869b9ea6c098
gunhaa <wh8299@gmail.com>
2025-01-29 02:34:45 +0900

    init

---
+++ src/main/java/Iterator/IteratorExecuter.java
@@ -0,0 +1,24 @@
+package Iterator;
+
+import java.util.Iterator;
+
+public class IteratorExecuter {
+    public static void main(String[] args) {
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("Bible"));
+        bookShelf.appendBook(new Book("Cinderella"));
+        bookShelf.appendBook(new Book("Gatsby"));
+        bookShelf.appendBook(new Book("BabyPig"));
+
+        Iterator<Book> it = bookShelf.iterator();
+        while(it.hasNext()){
+            Book book = it.next();
+            System.out.println("book.getName() = " + book.getName());
+        }
+        System.out.println("==================");
+
+        for(Book book : bookShelf){
+            System.out.println("book.getName() = " + book.getName());
+        }
+    }
+}
