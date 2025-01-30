9b9292ff1d6f30a6e55fed0857dfd79b06e7db46
gunhaa <wh8299@gmail.com>
2025-01-31 02:24:46 +0900

    feat : template method update

---
+++ src/main/java/templateMethod/AbstractDisplay.java
@@ -0,0 +1,15 @@
+package templateMethod;
+
+public interface AbstractDisplay {
+    public abstract void open();
+    public abstract void print();
+    public abstract void close();
+
+    public default void display(){
+        open();
+        for(int i=0; i<5; i++){
+            print();
+        }
+        close();
+    }
+}
---
+++ src/main/java/templateMethod/TemplateMethodExecutor.java
@@ -0,0 +1,14 @@
+package templateMethod;
+
+public class TemplateMethodExecutor {
+
+    public static void main(String[] args) {
+
+        AbstractDisplay d1 = new CharDisplay('c');
+
+        AbstractDisplay d2 = new StringDisplay("Hello, world");
+
+        d1.display();
+        d2.display();
+    }
+}
