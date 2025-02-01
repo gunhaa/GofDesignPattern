e939be53e3fbf16ddf093c1a27d1acc5d1c56b84
gunhaa <wh8299@gmail.com>
2025-02-02 03:04:27 +0900

    feat : add abstract factory

---
+++ src/main/java/abstractfactory/AboutAbstractFactory.java
@@ -0,0 +1,9 @@
+package abstractfactory;
+
+public class AboutAbstractFactory {
+    /*
+    추상적인 공장에서 추상적인 부품을 조합하여 추상적인 제품을 만드는 것
+    부품의 구체적인 구현에는 주목하지 않고 인터페이스(API)에 주목한다.
+    그리고 인터페이스(API)만 사용해서 부품을 조립하고 제품으로 완성하는 것이다
+    */
+}
