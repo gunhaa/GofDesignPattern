0ea96e5c2586538b05f22e36806e8736e70ae508
gunha <wh8299@gmail.com>
2025-02-02 14:10:28 +0900

    feat : add composite

---
+++ src/main/java/composite/AboutComposite.java
@@ -0,0 +1,8 @@
+package composite;
+
+public class AboutComposite {
+    /*
+    Composite(컴포지트) 패턴은 객체들을 트리 구조로 구성하여 개별 객체와 그룹을 동일하게 다룰 수 있도록 하는 패턴이다.
+    즉, 단일 객체(Leaf)와 복합 객체(Composite)를 동일한 인터페이스로 다룰 수 있게 한다.
+    */
+}
