9eea18c485ff3ae9f9c64be5c4d3d183aff3237e
gunhaa <wh8299@gmail.com>
2025-02-07 00:48:40 +0900

    feat : update prototype

--- src/main/java/prototype/AboutPrototype.java
+++ src/main/java/prototype/AboutPrototype.java
@@ -8,6 +8,20 @@ public class AboutPrototype {
     2. 클래스로 부터 인스턴스 생성이 힘든 경우(ex, 마우스로 도형을 그리는 어플리케이션)
     3. 프레임워크와 생성하는 인스턴스를 분리하고 싶은 경우
         - 클래스 이름 대신 strong message나 slash box 문자열이 인스턴스 생성을 위한 이름으로 부여되었다.
+
+    자바는 prototype 디자인 패턴을 clonable을 extends 한 clone()메서드를 통해 구현가능하다.
+    자바의 clone()은 얕은 복사이며, 객체 자체는 새로운 메모리 주소를 가지지만, 참조형 필드는 같은 주소를 참조하게 된다.
+    ex)
+        public class Product implements Cloneable {
+            private String name;
+            private ProductDetails details;  // 참조형 필드
+            ..
+        p1 주소: Product@6d06d69c
+        p1Clone 주소: Product@7852e922
+        p1.details 주소: ProductDetails@7852e922
+        p1Clone.details 주소: ProductDetails@7852e922
+        p1 == p1Clone? false
+        p1.details == p1Clone.details? true
     */
 
 }
--- src/main/java/prototype/PrototypeExecutor.java
+++ src/main/java/prototype/PrototypeExecutor.java
@@ -16,6 +16,9 @@ public class PrototypeExecutor {
         manager.register("slash box", sbox);
 
         Product p1 = manager.create("strong message");
+        System.out.println(p1 == uPen);
+        Product p1Clone = uPen.createCopy();
+        System.out.println(p1 == p1Clone);
         p1.use("hello, world");
         Product p2 = manager.create("warning box");
         p2.use("hello, world");
